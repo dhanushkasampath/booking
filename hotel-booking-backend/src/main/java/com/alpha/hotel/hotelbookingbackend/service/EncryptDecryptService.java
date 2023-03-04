3976c9723a0101c748e3b47306c83552bfc55634
dhanushka <dhanushka.sampath@madmobile.com>
2023-03-04 10:05:03 +0530

    encrypt decrypt completed

--- hotel-booking-backend/src/main/java/com/alpha/hotel/hotelbookingbackend/service/EncryptDecryptService.java
+++ hotel-booking-backend/src/main/java/com/alpha/hotel/hotelbookingbackend/service/EncryptDecryptService.java
@@ -3,5 +3,7 @@ package com.alpha.hotel.hotelbookingbackend.service;
 import com.alpha.hotel.hotelbookingbackend.exception.HotelBookingException;
 
 public interface EncryptDecryptService {
-    String encrypt(String providedPassword, String secretKey) throws HotelBookingException;
+    String encrypt(String stringTobeEncrypted, String secretKey) throws HotelBookingException;
+
+    String decrypt(String stringTobeDecrypted, String secretKey) throws HotelBookingException;
 }
--- hotel-booking-backend/src/main/java/com/alpha/hotel/hotelbookingbackend/service/impl/EncryptDecryptServiceImpl.java
+++ hotel-booking-backend/src/main/java/com/alpha/hotel/hotelbookingbackend/service/impl/EncryptDecryptServiceImpl.java
@@ -12,30 +12,45 @@ import javax.crypto.spec.SecretKeySpec;
 import java.nio.charset.StandardCharsets;
 import java.util.Base64;
 
+import static javax.crypto.Cipher.DECRYPT_MODE;
+
 @Service
 public class EncryptDecryptServiceImpl implements EncryptDecryptService {
     private final Logger logger = LoggerFactory.getLogger(EncryptDecryptServiceImpl.class);
     private SecretKeySpec secretKeySpec;
-    public void setKey(String myKey) throws HotelBookingException {
+
+    @Override
+    public String encrypt(String stringTobeEncrypted, String secretKey) throws HotelBookingException {
         try {
-            secretKeySpec = new SecretKeySpec(myKey.getBytes(StandardCharsets.UTF_8), "AES");
-        }
-        catch ( Exception e ) {
-            logger.error("Error occurred while creating secret key:", e);
-            throw new HotelBookingException(HttpStatus.INTERNAL_SERVER_ERROR, "Error occurred while creating secret key");
+            setKey(secretKey);
+            Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5PADDING");
+            cipher.init(Cipher.ENCRYPT_MODE, secretKeySpec);
+            return Base64.getEncoder().encodeToString(cipher.doFinal(stringTobeEncrypted.getBytes(StandardCharsets.UTF_8)));
+        } catch (Exception e) {
+            logger.error("Error occurred while encrypting:", e);
+            throw new HotelBookingException(HttpStatus.INTERNAL_SERVER_ERROR, "Error occurred while encrypting");
         }
     }
 
-    public String encrypt(String providedPassword, String secretKey) throws HotelBookingException {
+    @Override
+    public String decrypt(String stringTobeDecrypted, String secretKey) throws HotelBookingException {
         try {
             setKey(secretKey);
-            Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
-            cipher.init(Cipher.ENCRYPT_MODE, secretKeySpec);
-            return Base64.getEncoder().encodeToString(cipher.doFinal(providedPassword.getBytes(StandardCharsets.UTF_8)));
+            Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5PADDING");
+            cipher.init(DECRYPT_MODE, secretKeySpec);
+            return new String(cipher.doFinal(Base64.getDecoder().decode(stringTobeDecrypted)));
+        } catch (Exception e) {
+            logger.error("Error occurred while decrypting:", e);
+            throw new HotelBookingException(HttpStatus.INTERNAL_SERVER_ERROR, "");
         }
-        catch ( Exception e ) {
-            logger.error("Error occurred while encrypting:", e);
-            throw new HotelBookingException(HttpStatus.INTERNAL_SERVER_ERROR, "Error occurred while encrypting");
+    }
+
+    public void setKey(String myKey) throws HotelBookingException {
+        try {
+            secretKeySpec = new SecretKeySpec(myKey.getBytes(StandardCharsets.UTF_8), "AES");
+        } catch (Exception e) {
+            logger.error("Error occurred while creating secret key:", e);
+            throw new HotelBookingException(HttpStatus.INTERNAL_SERVER_ERROR, "Error occurred while creating secret key");
         }
     }
 }
