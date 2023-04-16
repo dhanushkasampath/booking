7e0591de5541e0cf5ade6428fe61fbca8da41bb3
dhanushka <dhanushka.sampath@madmobile.com>
2023-04-16 07:38:57 +0530

    reset password with new password done

--- hotel-booking-frontend/src/pages/UserPasswordResetPage.js
+++ hotel-booking-frontend/src/pages/UserPasswordResetPage.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
 import { useState } from 'react';
-// import axios from 'axios';
-// import jwt from 'jwt-decode';
+import axios from 'axios';
+import jwt from 'jwt-decode';
 
 import '../App.css'
 
@@ -9,36 +10,41 @@ export default function ResetPassword()  {
 
     const [enteredPassword, setEnteredPassword] = useState("");
     const[confirmPassword, setConfirmPassword] = useState("");
-    
-    // const[userName, setUserName] = useState("buddhimakaushalya.mtr@gmail.com");
-
 
     async function resetPassword(event){
+        event.preventDefault();
         console.log(enteredPassword);
         console.log(confirmPassword);
-        // setUrl(window.location.href);
-        const token = window.location.href.replace("http://localhost:3001/reset-password/","");
+
+        const token = window.location.href
+            .replace("http://localhost:3000/reset-password/","")
+            .split('?')[0];
         console.log(token);
 
+        const decoded = jwt(token);
+        console.log(decoded);
+
+        const email = decoded.email;
+        console.log(email);
+
         if(enteredPassword === confirmPassword){
-            
-            // alert("Passwords match");
+            alert("Passwords match");
             console.log(window.location.href); 
-            // try
-            // {
-            //     await axios.post("http://localhost:8088/api/v1/user/login?userLoginType=FORGET_PASSWORD_LOGIN",
-            //     {            
-            //         userName : userName,  
-            //         password: enteredPassword            
-            //     });
-            //     alert("User Password Updated Successfully. Please login with new password.");
-            //     setUserName("");
-            //     setEnteredPassword("")               
-            // }
-            // catch(err)
-            // {
-            //     alert("Failed to update password. Please try again");
-            // }
+            try
+            {
+                await axios.post("http://localhost:8088/api/v1/user/login?userLoginType=FORGET_PASSWORD_LOGIN",
+                {
+                    userName : email,
+                    password: enteredPassword
+                });
+                alert("User Password Updated Successfully. Please login with new password.");
+                setEnteredPassword("");
+                setConfirmPassword("");
+            }
+            catch(err)
+            {
+                alert("Failed to update password. Please try again");
+            }
         }else{
             alert("Passwords do not match!");
         }
@@ -46,7 +52,7 @@ export default function ResetPassword()  {
     return (
         <div className="text-center m-5-auto">
             <h2>Please reset your password here!</h2>
-            <form>
+            <form action="/login">
                 <p>
                     <label>New Password</label><br/>
                     <input type="password" name="newPassword" required 
@@ -65,8 +71,10 @@ export default function ResetPassword()  {
                         setConfirmPassword(event.target.value);      
                     }}/>
                 </p>
-                <p>                
-                    <button id="sub_btn" type="submit" onClick={resetPassword}>Reset</button>
+                <p>
+                    <Link to="/">
+                        <button id="sub_btn" type="submit" onClick={resetPassword}>Reset</button>
+                    </Link>
                 </p>
             </form>            
         </div>
