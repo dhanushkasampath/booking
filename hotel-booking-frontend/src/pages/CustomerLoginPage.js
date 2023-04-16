6dd98afa3792063231ea9a85209f613788c00aee
dhanushka <dhanushka.sampath@madmobile.com>
2023-04-16 09:58:21 +0530

    user login integration done

--- hotel-booking-frontend/src/pages/CustomerLoginPage.js
+++ hotel-booking-frontend/src/pages/CustomerLoginPage.js
@@ -1,25 +1,69 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-
-import '../App.css'
+import React from 'react';
+import {useState} from 'react';
+import {Link, useHistory} from 'react-router-dom';
+import '../App.css';
+import axios from "axios";
 
 export default function SignInPage() {
+    const history = useHistory();
+
+    const [userName, setUserName] = useState("");
+    const [password, setPassword] = useState("");
+
+    async function userLogin(event) {
+        event.preventDefault();
+        setUserName("");
+        setPassword("");
+        try {
+            await axios.post("http://localhost:8088/api/v1/user/login?userLoginType=GENERAL_LOGIN",
+                {
+                    userName: userName,
+                    password: password
+                }).then((response) => {
+                console.log(response.status);
+                console.log('RESPONSE: ' + response.data.token);
+
+                if (response.status === 200) {
+                    localStorage.setItem('token', response.data.token);
+
+                    //redirect to home page
+                    history.replace('/home');
+                } else {
+                    //stay in the same page
+                    alert("Invalid Credentials. Please try again");
+                }
+            });
+        } catch (err) {
+            alert("Failed login. Please try again");
+        }
+    }
+
     return (
         <div className="text-center m-5-auto">
             <h2>Sign in to us</h2>
             <form action="/home">
                 <p>
-                    <label>Username or email address</label><br/>
-                    <input type="text" name="first_name" required />
+                    <label>Username</label><br/>
+                    <input type="text" name="user_name" required
+                           value={userName}
+                           onChange={(event) => {
+                               setUserName(event.target.value);
+                           }}
+                    />
                 </p>
                 <p>
                     <label>Password</label>
                     <Link to="/forget-password"><label className="right-label">Forget password?</label></Link>
                     <br/>
-                    <input type="password" name="password" required />
+                    <input type="password" name="password" required
+                           value={password}
+                           onChange={(event) => {
+                               setPassword(event.target.value);
+                           }}
+                    />
                 </p>
                 <p>
-                    <button id="sub_btn" type="submit">Login</button>
+                    <button id="sub_btn" type="submit" onClick={userLogin}>Login</button>
                 </p>
             </form>
             <footer>
--- hotel-booking-frontend/src/pages/UserPasswordResetPage.js
+++ hotel-booking-frontend/src/pages/UserPasswordResetPage.js
@@ -1,23 +1,23 @@
 import React from 'react';
-import { Link } from 'react-router-dom'
-import { useState } from 'react';
+import {useHistory} from 'react-router-dom'
+import {useState} from 'react';
 import axios from 'axios';
 import jwt from 'jwt-decode';
 
 import '../App.css'
 
-export default function ResetPassword()  {
+export default function ResetPassword() {
+    const history = useHistory();
 
     const [enteredPassword, setEnteredPassword] = useState("");
-    const[confirmPassword, setConfirmPassword] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState("");
 
-    async function resetPassword(event){
+    async function resetPassword(event) {
         event.preventDefault();
-        console.log(enteredPassword);
-        console.log(confirmPassword);
-
+        setEnteredPassword("");
+        setConfirmPassword("");
         const token = window.location.href
-            .replace("http://localhost:3000/reset-password/","")
+            .replace("http://localhost:3000/reset-password/", "")
             .split('?')[0];
         console.log(token);
 
@@ -27,56 +27,60 @@ export default function ResetPassword()  {
         const email = decoded.email;
         console.log(email);
 
-        if(enteredPassword === confirmPassword){
-            alert("Passwords match");
-            console.log(window.location.href); 
-            try
-            {
+        if (enteredPassword === confirmPassword) {
+            console.log("Passwords match");
+            console.log(window.location.href);
+            try {
                 await axios.post("http://localhost:8088/api/v1/user/login?userLoginType=FORGET_PASSWORD_LOGIN",
-                {
-                    userName : email,
-                    password: enteredPassword
+                    {
+                        userName: email,
+                        password: enteredPassword
+                    }).then((response) => {
+                    console.log(response.status);
+                    console.log('RESPONSE: ' + response.data.token);
+
+                    if (response.status === 200) {
+                        alert("User Password Updated Successfully. Please login with new password.");
+
+                        //redirect to home page
+                        history.replace('/');
+                    } else {
+                        //stay in the same page
+                        alert("Failed to update password. Please try again");
+                    }
                 });
-                alert("User Password Updated Successfully. Please login with new password.");
-                setEnteredPassword("");
-                setConfirmPassword("");
-            }
-            catch(err)
-            {
+            } catch (err) {
                 alert("Failed to update password. Please try again");
             }
-        }else{
+        } else {
             alert("Passwords do not match!");
         }
     }
+
     return (
         <div className="text-center m-5-auto">
             <h2>Please reset your password here!</h2>
             <form action="/login">
                 <p>
                     <label>New Password</label><br/>
-                    <input type="password" name="newPassword" required 
-                    value={enteredPassword}
-                    onChange={(event) =>
-                    {
-                        setEnteredPassword(event.target.value);      
-                    }}/>
+                    <input type="password" name="newPassword" required
+                           value={enteredPassword}
+                           onChange={(event) => {
+                               setEnteredPassword(event.target.value);
+                           }}/>
                 </p>
                 <p>
-                    <label>Confirm Password</label>                    
-                    <input type="password" name="confirmPassword" required 
-                    value={confirmPassword}
-                    onChange={(event) =>
-                    {
-                        setConfirmPassword(event.target.value);      
-                    }}/>
+                    <label>Confirm Password</label>
+                    <input type="password" name="confirmPassword" required
+                           value={confirmPassword}
+                           onChange={(event) => {
+                               setConfirmPassword(event.target.value);
+                           }}/>
                 </p>
                 <p>
-                    <Link to="/">
-                        <button id="sub_btn" type="submit" onClick={resetPassword}>Reset</button>
-                    </Link>
+                    <button id="sub_btn" type="submit" onClick={resetPassword}>Reset</button>
                 </p>
-            </form>            
+            </form>
         </div>
     )
 }
